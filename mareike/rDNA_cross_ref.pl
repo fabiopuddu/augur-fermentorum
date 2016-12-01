2e832a187cbaba6081004ed75dc98b6197a3f869
Mareike Herzog <mh588@cb-head2.gurdon.private.cam.ac.uk>
2016-12-01 21:54:41 +0000

    Changed and wrote scripts for cross-referencing gene lists

--- mareike/rDNA_cross_ref.pl
+++ mareike/rDNA_cross_ref.pl
@@ -1,15 +1,15 @@
-#!/usr/bin/env perl
 
 # Author:       mh23
 # Maintainer:   mh23
-# Created: 		Sep 2016
-# Name: mito_cross_ref.pl
+# Created: 		Dec 2016
+# Name: rDNA_cross_ref.pl
 
 
 use warnings;
 use strict;
 
 #my $SGD = shift;
+#input should be the lower/higher sig genes
 my $genelist = shift;
 
 #Open genelist 
@@ -30,10 +30,10 @@ open(F, $genelist ) or die ("Unable to open file $genelist: $!\n" );
 			}
            
            #check its existence in the mito_cand_genes_SGD.txt file  
-           my $command = "cat mito_cand_genes_SGD.txt | grep $gene -w";
+           my $command = "cat /mnt/scratch/jackson/fp305/results/rDNA_cand_genes_kobayashi.txt | grep $gene -w";
            my $result = `$command`;       
            if ($result eq ''){
-           	print "$gene\tNot_on_SGD\n";
+           	print "$gene\tNot_on_Kobayashi\n";
            }
            else{
            	print "$result";
