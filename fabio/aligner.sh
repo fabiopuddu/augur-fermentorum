38cb0c80e6da082ede22d01698bd38b333f65fb5
Fabio Puddu <fpuddu@gmail.com>
2017-06-16 17:48:13 +0100

    first draft of automatic aligner wrapper

--- fabio/aligner.sh
+++ fabio/aligner.sh
@@ -1,1 +1,11 @@
-bwa mem -R "@RG\tID:sample name\tSM:SC_MFY/SD/ERS" /Applications/PF/mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa fastq/SRR800790_1.fastq.gz fastq/SRR800790_2.fastq.gz | samtools view -bS - | samtools sort -o SC_MFY000000050.bam  -O bam -T temp
+#!/bin/bash
+submit_sbatch(){
+sbatch --partition=LONG --output /dev/null --error %j.err ${1} --wrap="${2}" 
+}
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) #get the directory this program is stored in
+cat ../experiments/name\ conversion.tsv | cut -f5 | sort | uniq | while read line
+	do 
+	command="bwa mem -R \"@RG\tID:$ERSNO\tSM:$SNAME\" $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa $line.fq1.gz $line.fq2.gz | samtools view -bS - | samtools sort -o $line.bam  -O bam -T $line.temp"
+#	echo $command
+	submit_sbatch "" "$command"
+	done
