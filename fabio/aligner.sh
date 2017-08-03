3f2c22a3c6d535b9ff61e01c44c9d473a12fa5bc
Fabio Puddu <fpuddu@gmail.com>
2017-08-03 09:26:02 +0100

    moved indexing to slurm queue

--- fabio/aligner.sh
+++ fabio/aligner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Declare function that checks if the current jobs are done
 waitforcompletion(){
 if [[ ${v} -eq '1' ]]; then printf "Waiting for jobs $1  to complete"; else printf "Waiting for jobs to complete"; fi
 list="ciao${1}"
@@ -11,28 +12,35 @@ while [[ $finito != '0' ]]
 printf "\n"    
 }
 
+#Declare function to submit jobs
 submit_sbatch(){
 sbatch --partition=LONG --output /dev/null --error %j.err ${1} --wrap="${2}" 
 }
+
+
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) #get the directory this program is stored in
 proclist=''
 while read -r line
 	do 
  	ERSNO=`cat ../experiments/name\ conversion.tsv | grep $line | head -n1 | cut -f7`
 	SNAME=`cat ../experiments/name\ conversion.tsv | grep $line | head -n1 | cut -f4`
-	command="bwa mem -R \"@RG\tID:${ERSNO}\tSM:${SNAME}\" $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa $line.fq1.gz $line.fq2.gz | samtools view -bS - | samtools sort -o $line.bam  -O bam -T $line.temp"
+	command1="bwa mem -R \"@RG\tID:${ERSNO}\tSM:${SNAME}\" $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa $line.fq1.gz $line.fq2.gz | samtools view -bS - | samtools sort -o $line.bam  -O bam -T $line.temp"
+	command2="samtools index $line.bam"
 	echo $command
-	PROC1=$(submit_sbatch "" "$command" |  sed 's/Submitted batch job //g')
-	proclist="${proclist}\|${PROC1}"
+	PROC1=$(submit_sbatch "" "$command1" |  sed 's/Submitted batch job //g')
+	PROC2=$(submit_sbatch "--dependency=afterok:${PROC1}" "$command2" |  sed 's/Submitted batch job //g')
+	proclist="${proclist}\|${PROC2}"
 	done  < <(cat ../experiments/name\ conversion.tsv | cut -f5 | sort | uniq)
+
 waitforcompletion "$proclist"
 
-proclist=''
-while read -r line
-	do command="samtools index $f"
-	PROC1=$(submit_sbatch "" "$command" |  sed 's/Submitted batch job //g')
-	done < <(ls *.bam)
-waitforcompletion "$proclist"
+#proclist=''
+#while read -r line
+#	do command="samtools index $f"
+#	PROC1=$(submit_sbatch "" "$command" |  sed 's/Submitted batch job //g')
+#	done < <(ls *.bam)
+#waitforcompletion "$proclist"
 
 
 
