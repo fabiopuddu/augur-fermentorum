fabiopuddu/augur-fermentorum

commit 5e5baa808ea89feae26b29bbe05cd0d2cbd0026d

fixed bug in analyser multi requiring a sample code had to contain SD or SC_MFY number

shell/analyser-multi.sh
@@ -209,7 +209,7 @@ if [[   $rDNA == 1 ]]
         	  cd repDNA
             while read -r line ; 
             do  
-               name=`echo $line | grep -o "SC_MFY.......\|SD......"| sed "s|\.||g" | head -n1`
+               name=`echo $line | cut -f5| sed "s|\.||g" | head -n1`
                command="rDNA-cnv_estimate.pl -p ${ploidy} -i ../$line > $name.txt" 
                if [[ ${v} -eq '1' ]]; then echo $command; fi
                PROC1=$(sbatch --partition=LONG  --wrap="${command}" | sed 's/Submitted batch job //g') 

commit fbcba6782661f0eaf92f1467695820a4b130532e

fixed bug introduced by commit 5e5baa8

shell/analyser-multi.sh
@@ -209,8 +209,8 @@ if [[   $rDNA == 1 ]]
         	  cd repDNA
             while read -r line ; 
             do  
-               name=`echo $line | cut -f5| sed "s|\.||g" | head -n1`
-               command="rDNA-cnv_estimate.pl -p ${ploidy} -i ../$line > $name.txt" 
+               name=`echo $line | tr "/" "\n" | tail -n1 | sed "s|\.bam||g" | head -n1`
+	      command="rDNA-cnv_estimate.pl -p ${ploidy} -i ../$line > $name.txt" 
                if [[ ${v} -eq '1' ]]; then echo $command; fi
                PROC1=$(sbatch --partition=LONG  --wrap="${command}" | sed 's/Submitted batch job //g') 
                proclist="${proclist}\|${PROC1}"