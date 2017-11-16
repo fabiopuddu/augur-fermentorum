8e9b41d144382241ece0b53f5df760c946621eeb
Inigo Ayestaran <ia327@cb-head2.gurdon.private.cam.ac.uk>
2017-11-16 14:22:58 +0000

    minor changes to bash scripts

--- shell/experiment-analyser.sh
+++ shell/experiment-analyser.sh
@@ -4,14 +4,14 @@
 	for folder in Del*
 		do 	
 			del_number=`echo $folder | tr '_' "\n" | head -n1` #Identify which deletion strain we are working on
-			control_samples=`cat name\ conversion.tsv | grep "$del_number"'_' | grep C0[0123456789][0123456789] | grep -o ERS.* | tr "\n" ',' | sed 's/,*$//' `  #identify the ERS numbers of the control samples.
-			control_samples=`cat name\ conversion.tsv | grep "$del_number"'_' | grep C0[0123456789][0123456789] | grep -o ERS.* | head -n1 | tr "\n" ',' | sed 's/,*$//' ` #added head -n1 to only get one control #identify the ERS numbers of the control samples.
+			control_samples=`cat name\ conversion.tsv | grep "$del_number"'_' | grep C[0123456789] | awk -F"\t" '$6 ~ ERS {print $6}' | sed -e 's/[[:space:]]*$//' | tr "\n" ',' | sed 's/,*$//' `  #identify the ERS numbers of the control samples.
+#            control_samples=`cat name\ conversion.tsv | grep "$del_number"'_' | grep C[0123456789] | awk -F"\t" '$6 ~ ERS {print $6}' | sed -e 's/[[:space:]]*$//' | head -n1 | tr "\n" ',' | sed 's/,*$//' ` #added head -n1 to only get one control #identify the ERS numbers of the control samples.
 			echo $control_samples
 			cd $folder
 			if [[ -a bams_for_mpileup ]]
 				then	if [[ $control_samples == '' ]] 
-								then analyser-multi.sh -a -r -n2 -F -x  >> results.txt &#$folder
-								else analyser-multi.sh -a -r -n2 -F -c $control_samples >> results.txt &#$folder
+								then analyser-multi.sh -a -r -n2 -x -F >> results.txt &#$folder
+								else analyser-multi.sh -a -r -n2 -F -C $control_samples >> results.txt &#$folder
 						fi
 			fi
 			cd ..
--- shell/extract-fastq.sh
+++ shell/extract-fastq.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 submit_sbatch(){
 sbatch --partition=LONG --output /dev/null --error /dev/null ${1} --wrap="${2}" 
 }
@@ -18,20 +20,20 @@ printf "\n"
 proclist=''
 while read -r line
  do              fl=`echo $line | sed "s/.bam//g"`
-                echo $fl 
-		command1="samtools sort -n -T ${fl}_temp.bam -O BAM -o ${fl}_sorted.bam ${fl}.bam"
+                echo $fl
+                 command1="samtools sort -n -T ${fl}_temp.bam -O BAM -o ${fl}_sorted.bam ${fl}.bam"
                  command2="bamToFastq -i ${fl}_sorted.bam -fq ${fl}.fq1 -fq2 ${fl}.fq2"
                  command3="gzip -9 -f ${fl}.fq1 ${fl}.fq2"
                  command4="rm ${fl}_sorted.bam"
-                 PROC1=$(submit_sbatch " " "${command1}" | sed 's/Submitted batch job //g') 
+                 PROC1=$(submit_sbatch " " "${command1}" | sed 's/Submitted batch job //g')
                  PROC2=$(submit_sbatch "--dependency=afterok:${PROC1}" "${command2}"  | sed 's/Submitted batch job //g')
                  PROC3=$(submit_sbatch "--dependency=afterok:${PROC2}" "${command3}"  | sed 's/Submitted batch job //g')
                  PROC4=$(submit_sbatch "--dependency=afterok:${PROC3}" "${command4}"  | sed 's/Submitted batch job //g')
-		 proclist="${proclist}\|${PROC4}"
+         proclist="${proclist}\|${PROC4}"
 		 sleep 1
  done  < <(ls *.bam)
 
-waitforcompletion "${proclist}" 
+waitforcompletion "${proclist}"
 
 
 for f in *.bam; 
