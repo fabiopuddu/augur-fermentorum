c388f39f893018ca58aa54acd32a0eb3ddcacb9f
Fabio Puddu <fpuddu@gmail.com>
2018-02-12 23:21:14 +0000

    removed a lot of commented lines and commands not relevant anymore

--- shell/variant_caller.sh
+++ shell/variant_caller.sh
@@ -78,12 +78,7 @@ if [[ -a bams_for_mpileup ]]
     then rm bams_for_mpileup
 fi
 #create a list of BAM files to analyse
-#ls BAM/*/*/*/*.bam >> bams_for_mpileup
-#ls BAM/*/*/*.bam >> bams_for_mpileup
-#	ls BAM/*/*.bam >> bams_for_mpileup
 ls BAM/*.bam >> bams_for_mpileup
-sort bams_for_mpileup > sorted_bams_for_mpileup
-#cat sorted_bams_for_mpileup | grep -v 'sorted' > sieved_bams_for_mpileup #remove'sorted' files, which come from re-alignments for ty and rDNA analysis
 #index files if the index does not exist
 cat bams_for_mpileup | parallel -j20 --no-notice ' x={};
 				n=`echo {} | tr '/' "\n" | tail -n1 | sed 's/.bam//g'`; 
@@ -92,32 +87,6 @@ cat bams_for_mpileup | parallel -j20 --no-notice ' x={};
                 				then    samtools index $x
         				fi'
 printf "\n"
-#cat sieved_bams_for_mpileup | grep 'merged' > merged_bams_for_mpileup #generate a list of already merged BAM files
-#remove from sieved bams for mpileup any reference to files that have already been merged
-#cat sieved_bams_for_mpileup | grep 'merged' | grep -o "SC_MFY.......\|SD....." | sort -u | while read line
-#	do cat sieved_bams_for_mpileup | sed "/$line/d" > new_bams_for_mpileup
-#	   mv new_bams_for_mpileup sieved_bams_for_mpileup
-#	done	
-#combine the lists of unmerged samples with the list of already merged samples	
-#cat sieved_bams_for_mpileup merged_bams_for_mpileup > bams_for_mpileup	
-#rm sieved_bams_for_mpileup merged_bams_for_mpileup sorted_bams_for_mpileup	
-#I don't understand what the following lines are for: should have commented
-# cat bams_for_mpileup | while read line
-#     do if echo $line | grep -q 'merged.bam'
-#             echo $line >> new_bams_for_mpileup
-#         fi
-#     done
-#####################################################################
-##### MERGE MULTIPLE BAM FILES IF SAMPLES WERE SEQUENCED TWICE#######
-#####################################################################
-#temporarily commented these out as they are not relevant anymore
-#sort bams_for_mpileup > sorted_bams_for_mpileup
-#merge_bams_samtools.pl sorted_bams_for_mpileup 
-#waitforcompletion
-#rm bams_for_mpileup
-#mv sorted_bams_for_mpileup bams_for_mpileup
-#cat bams_for_mpileup | grep merged | parallel --no-notice 'samtools index {}' #re-index the merged files
-
 #generate igv.txt
 sed 's|^BAM|load "/Volumes/LuCia/BAM files/Yeast|' bams_for_mpileup > igv.txt 
 sed 's|$|"|' igv.txt > igv_loader.txt
@@ -134,7 +103,6 @@ if [[ $check_deletions == 1 ]]
 						name=`echo $line | grep -oE '\bSD[^ ]*\.|\bSC_MFY[^ ]*\.' | tr -d '.'`
 						printf "Gene: $gene_name\tProcessing...$name\t" >>../deletion_check_log
 						check=`detect_deletion_chr_region.pl $gene_name $line | tr "\t" "\n" | grep Deleted: | sed 's/Deleted://g'`
-						#check=`detect_deletion_chr_region.pl $gene_name $line`
 						printf "$check\n" >>../deletion_check_log
 						if [[ $check == 'Y' || $check == 'Yp (p=partial)' ]] 
 							then echo $line >> bams_for_mpileup_filtered
@@ -181,7 +149,7 @@ cat ../bams_for_mpileup | while read line
                 else
                     if [ "$used_caller" == "S" ]
                         then sbatch --partition=LONG -o slurm.%N.%j.out.txt -e slurm.%N.%j.err.txt --wrap="samtools mpileup -f $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa -g -t DP,DV -C0 -pm3 -F0.2 -d10000 ../$line | bcftools call -vm -f GQ | bgzip -f > $ers.vcf.gz"
-                        elif [ "$used_caller" == "F" ]
+                    elif [ "$used_caller" == "F" ]
                         then sbatch --partition=LONG -o slurm.%N.%j.out.txt -e slurm.%N.%j.err.txt --wrap="freebayes -f $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa -p $ploidy -m0 -C3 -F $minfrac --max-coverage 10000 -J -= ../$line | bgzip -f > $ers.vcf.gz"
                     fi
             fi
