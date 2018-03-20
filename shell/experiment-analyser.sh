b6595582eaaa28e797ec21b420f51bd33326ed3e
Fabio Puddu <fpuddu@gmail.com>
2018-03-20 17:28:05 +0000

    added commenting to explain which line to uncomment to use different controls

--- shell/experiment-analyser.sh
+++ shell/experiment-analyser.sh
@@ -3,9 +3,14 @@
 #then if a proper calling has been done, runs the analyser with standard options
 	for folder in Del*
 		do 	
+			echo $folder
 			del_number=`echo $folder | tr '_' "\n" | head -n1` #Identify which deletion strain we are working on
-			control_samples=`cat name\ conversion.tsv | grep "$del_number"'_' | grep C[0123456789] | awk -F"\t" '$6 ~ ERS {print $6}' | sed -e 's/[[:space:]]*$//' | tr "\n" ',' | sed 's/,*$//' `  #identify the ERS numbers of the control samples.
-#            control_samples=`cat name\ conversion.tsv | grep "$del_number"'_' | grep C[0123456789] | awk -F"\t" '$6 ~ ERS {print $6}' | sed -e 's/[[:space:]]*$//' | head -n1 | tr "\n" ',' | sed 's/,*$//' ` #added head -n1 to only get one control #identify the ERS numbers of the control samples.
+		#	Uncoment the next line to use any strain marked as C in the plate field as control
+		#	control_samples=`cat name\ conversion.tsv | grep "${del_number}_" | grep C[0123456789] | awk -F"\t" '$6 ~ ERS {print $6}' | sed -e 's/[[:space:]]*$//' | tr "\n" ',' | sed 's/,*$//' `  #identify the ERS numbers of the control samples.
+		#	Uncomment the next line to use only the *first* strain  marked as C in the plate field as control
+		#      control_samples=`cat name\ conversion.tsv | grep "${del_number}_" | grep C[0123456789] | awk -F"\t" '$6 ~ ERS {print $6}' | sed -e 's/[[:space:]]*$//' | head -n1 | tr "\n" ',' | sed 's/,*$//' ` #added head -n1 to only get one control #identify the ERS numbers of the control samples.
+		#	Uncomment the next line to always use the same wild-type control for everything
+			control_samples='ERS1042082'
 			echo $control_samples
 			cd $folder
 			if [[ -a bams_for_mpileup ]]
