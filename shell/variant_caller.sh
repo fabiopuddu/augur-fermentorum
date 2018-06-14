322266a079b3715f9abbacd89e0731ae0b28cda4
Fabio Puddu <fpuddu@gmail.com>
2018-06-14 17:17:30 +0100

    simplified and improved speed of variant caller + partially moved to submit_sbatch concept

--- shell/variant_caller.sh
+++ shell/variant_caller.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# Author:       Fabio Puddu  
+# Maintainer:   Fabio Puddu
+# Created:      Jan 2015
+# Description:
+
+
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 force_rewrite=0; v=0; check_deletions=0;DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); f=0;
-caller_chosen=0
+used_caller=''
 #################################
 #                               #
 #   GET COMMAND LINE OPTIONS    #
@@ -28,76 +34,54 @@ while getopts "fvkhSF" opt
                         ;;
                         S)
                         used_caller="S"
-                        caller_chosen=1
                         ;;
                         F)
                         used_caller="F"
-                        caller_chosen=1
                         ;;
         esac
     done
 
 ## Default variant caller and check
-if [ "$caller_chosen" == 0 ]
+if [ "$used_caller" == '' ]
     then used_caller="S"
     printf "samtools mpileup chosen as variant caller by default\n"
-fi
-
-#if [ "$used_caller" != "S" ] && [ "$used_caller" != "F" ]
-#    then echo "The variant caller (-c) should be S (samtools mpileup) or F (freebayes)"
-#    exit 1
-#fi
-
-if [ "$used_caller" == "S" ] && [ "$caller_chosen" == 1 ]
+elif [ "$used_caller" == "S" ]
     then echo "samtools mpileup chosen as variant caller"
-fi
-
-if [ "$used_caller" == "F" ] && [ "$caller_chosen" == 1 ]
+elif [ "$used_caller" == "F" ]
     then echo "freebayes chosen as variant caller"
 fi
 
 #######################
 #######PROGRAM STARTS##
 #############################
-waitforcompletion(){
-printf "Waiting for process to complete"
-finito=`squeue -u ia327 | wc -l | tr -d "\t"`
-while [[ $finito != '1' ]]    
-    do  finito=`squeue -u ia327 | wc -l | tr -d "\t"`
-        printf '.'
-        sleep 5
-    done  
-printf "\n"    
-}
 
 ###########################################################################################
 ###### GET LIST OF BAM FILES TO PROCESS AND INDEX THEM IF THE BAI FILES ARE MISSING########
 ###########################################################################################
 #cleanup
-if [[ -a bams_for_mpileup ]]
-    then rm bams_for_mpileup
-fi
+rm -f bams_for_mpileup
 #create a list of BAM files to analyse
 ls BAM/*.bam >> bams_for_mpileup
 #index files if the index does not exist
-cat bams_for_mpileup | parallel -j20 --no-notice ' x={};
-				n=`echo {} | tr '/' "\n" | tail -n1 | sed 's/.bam//g'`; 
+cat bams_for_mpileup | parallel -j20 --no-notice '
+				x={};
+				n=`echo $x | tr '/' "\n" | tail -n1 | sed 's/.bam//g'`; 
 	    			printf "\r\033[K  Indexing $n"
 		 		if [[ ! -a $x.bai || $force_rewrite == 1  ]] #if the file does not exist or if force_rewrite is called
-                				then    samtools index $x
-        				fi'
+                			then samtools index $x
+        			fi'
 printf "\n"
 #generate igv.txt
-sed 's|^BAM|load "/Volumes/LuCia/BAM files/Yeast|' bams_for_mpileup > igv.txt 
-sed 's|$|"|' igv.txt > igv_loader.txt
-rm igv.txt
+#sed 's|^BAM|load "/Volumes/LuCia/BAM files/Yeast|' bams_for_mpileup > igv.txt 
+#sed 's|$|"|' igv.txt > igv_loader.txt
+#rm igv.txt
 ###################################################################################################################
 #IF DELETION CHECK IS ENFORCED, CHECK THAT THE STRAIN IS DELETED IN THE GENE, AND EXCLUDE THE BAM FILE IF NOT
 ###################################################################################################################
 if [[ $check_deletions == 1 ]] 
 	then	touch bams_for_mpileup_filtered
 			printf "Checking deletions\n"
-			gene_name=`echo $PWD | tr '/' "\n" | grep Del | sed 's/Del[01234567.89]*_//g' | tr '[:lower:]' '[:upper:]'` 
+			gene_name=`echo $PWD | tr '/' "\n" | grep Del | sed 's/Del[01234567.89]*_//g' | sed 's/_.*$//g' | tr '[:lower:]' '[:upper:]'` 
 				cat bams_for_mpileup | while read line
 					do
 						name=`echo $line | grep -oE '\bSD[^ ]*\.|\bSC_MFY[^ ]*\.' | tr -d '.'`
@@ -118,10 +102,10 @@ cd calling
 cat ../bams_for_mpileup | while read line
 	do	n=$(echo $line |  tr '/' "\n" | tail -n1 | sed "s/\.bam//g" | sed "s/\.merged//g")
 		ers=`grep -w $n ../../name\ conversion.tsv | awk '{print $6}'`
-        check_pool=`echo $n | grep 'pool'`
+        	check_pool=`echo $n | grep 'pool'`
         if [ -z $check_pool ]
-            then ploidy=2
-            else ploidy=4
+            	then ploidy=2
+            	else ploidy=4
         fi
 		printf "\r\033[K  Processing $ers\t"
 #		if [[ ! -a $m.vcf.gz ]]
@@ -130,37 +114,33 @@ cat ../bams_for_mpileup | while read line
         minfrac=`bc -l <<< "scale=1; 2/$ploidy/5"` # Bash doesn't do floating point arithmetic
         minfrac=`echo "0$minfrac"`
 
+	#What caller are we using?
+	if [ "$used_caller" == "S" ]
+		then export SBATCH_CMD="samtools mpileup -f $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa -g -t DP,DV -C0 -pm3 -F0.2 -d10000 ../$line | bcftools call -vm -f GQ | bgzip -f > $ers.vcf.gz"
+	 elif [ "$used_caller" == "F" ]
+                then export SBATCH_CMD="freebayes -f $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa -p $ploidy -m0 -C3 -F $minfrac --max-coverage 10000 -E-1 -J -= $n.no2m.bam | bgzip -f > $ers.vcf.gz"
+        fi
+	
         ### Check if bam contains 2-micron reads
-        hits2m=`samtools view ../$line | awk '$3 == "2-micron"' | wc -l` # Optimise? (stop checking if one 2-micron is found)
-            if [ "$hits2m" -gt 0 ]
+        hits2m=`samtools idxstats ../$line | cut -f1 | grep "2-micron" | wc -l` # Optimise? (stop checking if one 2-micron is found)
+        if [ "$hits2m" -gt 0 ]
                 then
-                    samtools view -h ../$line | awk '!($3 == "2-micron")' > $n.no2m.sam
+                    #samtools view -h ../$line | awk '!($3 == "2-micron")' > $n.no2m.sam
                     command2="samtools view -Sb $n.no2m.sam | samtools sort -o $n.no2m.bam"
-                    if [ "$used_caller" == "S" ]
-                        then command3="samtools mpileup -f $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa -g -t DP,DV -C0 -pm3 -F0.2 -d10000 ../$line | bcftools call -vm -f GQ | bgzip -f > $ers.vcf.gz"
-                        elif [ "$used_caller" == "F" ]
-                        then command3="freebayes -f $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa -p $ploidy -m0 -C3 -F $minfrac --max-coverage 10000 -E-1 -J -= $n.no2m.bam | bgzip -f > $ers.vcf.gz"
-                    fi
                     command4="rm $n.no2m.*"
                     PROC2=$(sbatch --partition=LONG --wrap="${command2}" | sed 's/Submitted batch job //g')
-                    PROC3=$(sbatch --partition=LONG -o slurm.%N.%j.out.txt -e slurm.%N.%j.err.txt --dependency=afterok:${PROC2} --wrap="${command3}" | sed 's/Submitted batch job //g')
+                    PROC3=$(sbatch --dependency=afterok:${PROC2} submit_sbatch_caller.sh | sed 's/Submitted batch job //g')
                     sbatch --partition=LONG --dependency=afterok:${PROC3} --wrap="${command4}"
+		    export SBATCH_CMD=''
 
                 else
-                    if [ "$used_caller" == "S" ]
-                        then sbatch --partition=LONG -o slurm.%N.%j.out.txt -e slurm.%N.%j.err.txt --wrap="samtools mpileup -f $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa -g -t DP,DV -C0 -pm3 -F0.2 -d10000 ../$line | bcftools call -vm -f GQ | bgzip -f > $ers.vcf.gz"
-                    elif [ "$used_caller" == "F" ]
-                        then sbatch --partition=LONG -o slurm.%N.%j.out.txt -e slurm.%N.%j.err.txt --wrap="freebayes -f $DIR/../mpileup_defaults/reference_genome/Saccharomyces_cerevisiae.EF4.69.dna_sm.toplevel.fa -p $ploidy -m0 -C3 -F $minfrac --max-coverage 10000 -J -= ../$line | bgzip -f > $ers.vcf.gz"
-                    fi
+		   sbatch  submit_sbatch_caller.sh
+		   export SBATCH_CMD=''
             fi
 
 # freebayes outputs to vcf instead of bcf (no equivalent to -g mpileup tag)
 # freebayes can't choose just two tags for output (-t DP,AD/DV)
-
-			sleep 1
-		 	#I set the -C flag from 50 to 0 because when many artificially introduced mutantions fall within the same read, it downgraded mapping quality too much resulting in those 			mutations not being called.					
- #		fi
+	sleep 0.3
+#I set the -C flag from 50 to 0 because when many artificially introduced mutantions fall within the same read, it downgraded mapping quality too much resulting in those mutations not being called.					
  	done
- printf "\nDone\n"	
-#  Created by Fabio on 25/01/2015.
-#
+printf "\nDone\n"	
