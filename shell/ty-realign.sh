06e03661f98f8e028f8b92c71c766dde475fbf9e
Fabio Puddu <fpuddu@gmail.com>
2018-11-10 12:22:27 +0000

    simplified realignment and included duplicate marking with biobambam2

--- shell/ty-realign.sh
+++ shell/ty-realign.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 
@@ -17,21 +18,12 @@ cat bams_for_mpileup | while read line
  	do 	percorso=`echo ${line} | sed "s|/.*\.bam|/|g"`
 		name=`echo ${line} | tr '/' "\n" | tail -n1 | sed "s/\.bam//g"`
 
-		export SBATCH_CMD="bwa mem -R \"@RG\tID:$name\tSM:$name\" $DIR/../mpileup_defaults/repDNA_ref/repDNA.fa $percorso$name.fq1.gz $percorso$name.fq2.gz | samtools view -bS - | samtools sort -o TR_BAMS/$name.Ty.bam -O bam -T $name-ty"
+		export SBATCH_CMD="bwa mem -R \"@RG\tID:$name\tSM:$name\" $DIR/../mpileup_defaults/repDNA_ref/repDNA.fa $percorso$name.fq1.gz $percorso$name.fq2.gz | samtools view -b -F 4 - | bamsormadup threads=16 SO=coordinate level=0 verbose=0 fixmate=1 adddupmarksupport=1 tmpfile=$name.tmp | samtools sort > TR_BAMS/$name.Ty.bam"
 		PROC=$(sbatch submit_sbatch_ty_realign.sh | sed 's/Submitted batch job //g') 
 		
 		export SBATCH_CMD="samtools index TR_BAMS/$name.Ty.bam"
 		PROC2=$(sbatch --dependency=afterok:${PROC} submit_sbatch_ty_realign.sh | sed 's/Submitted batch job //g') 
 		
-		export SBATCH_CMD="samtools view -b -F 4 TR_BAMS/$name.Ty.bam > TR_BAMS/$name.Ty.bam.map.bam"
-		PROC3=$(sbatch --dependency=afterok:${PROC2} submit_sbatch_ty_realign.sh | sed 's/Submitted batch job //g')
-		
-		export SBATCH_CMD="mv TR_BAMS/$name.Ty.bam.map.bam TR_BAMS/$name.Ty.bam"
-		PROC4=$(sbatch --dependency=afterok:${PROC3} submit_sbatch_ty_realign.sh | sed 's/Submitted batch job //g')
-
-		export SBATCH_CMD="samtools index TR_BAMS/$name.Ty.bam"
-		sbatch --dependency=afterok:${PROC4} submit_sbatch_ty_realign.sh
-
 		export SBATCH_CMD=""
 
 #		PROC=$(sbatch --partition=LONG --wrap="${command1}" | sed 's/Submitted batch job //g') 
