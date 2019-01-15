dd1319541cae0b3ad7f7661a7152787f8deeee3a
Fabio Puddu <fpuddu@gmail.com>
2019-01-15 22:25:58 +0000

    included option of not doing deletion check when analysing whole experiments in shell/experiment-caller.sh

--- shell/experiment-caller.sh
+++ shell/experiment-caller.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 caller=""
+delcheck=0
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-while getopts "SF" opt
+while getopts "SFhnk" opt
     do  case "$opt" in
+	   h)  printf "############   HELP   ###############\nOPTIONS\n"
+                            printf "\t-h\tThis Help\n"
+                            printf "\t-k\tPerform gene KO check in folders\n"
+                            printf "\t-n\tDo not perform gene KO check in folders\n"
+                            printf "\t-S\tUse Samtools as variant caller\n"
+			    printf "\t-S\tUse Freebayes as variant caller\n"
+                            exit 0
+	    ;;
+	    k)
+	    delcheck='yes';
+	    ;;
+	    n)
+	    delcheck='no';
+	    ;;
             S)
             caller='-S'
             ;;
@@ -12,8 +27,10 @@ while getopts "SF" opt
             ;;
 esac
 done
-
-
+if [ $delcheck == 0 ]
+	then experiment-caller.sh -h  
+	     exit 0
+fi
 
 for directory in Del*
     do  cd $directory
@@ -23,7 +40,10 @@ for directory in Del*
         if [ -z $wt_name ]
             then
                 printf "Mutant sample\n"
-                export SBATCH_CMD_CIAO="variant_caller.sh -k $caller" #the k option enforce a check of the deletion before calling the mutations
+                if [ $delcheck == 'yes' ]
+			then export SBATCH_CMD_CIAO="variant_caller.sh -k $caller" #the k option enforce a check of the deletion before calling the mutations
+			else export SBATCH_CMD_CIAO="variant_caller.sh $caller"
+		fi
             else
                 printf "Wild type sample\n"
                 export SBATCH_CMD_CIAO="variant_caller.sh $caller"      # No deletion check for wild type samples
